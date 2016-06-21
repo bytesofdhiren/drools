b2bb70fb9f591f11b7f39ab4db884372a1c64bc2
Ubuntu <ubuntu@ip-172-31-47-117.ap-southeast-2.compute.internal>
2016-06-21 20:14:45 +1000

    updated docker images

--- kie-server/showcase/etc/start_kie-server.sh
+++ kie-server/showcase/etc/start_kie-server.sh
@@ -9,12 +9,13 @@ echo "Using '$KIE_SERVER_ID' as KIE server identifier"
 # If this KIE execution server container is linked with some KIE Workbench container, the following environemnt variables will be present, so configure the application arguments based on their values.
 if [ -n "$KIE_WB_PORT_8080_TCP" ] &&  [ -n "$KIE_WB_ENV_KIE_CONTEXT_PATH" ] &&  [ -n "$KIE_WB_PORT_8080_TCP_ADDR" ]; then
     # Obtain current container's IP address.
-    DOCKER_IP=$(ip addr show eth0 | grep -E '^\s*inet' | grep -m1 global | awk '{ print $2 }' | sed 's|/.*||')
+    #DOCKER_IP=$(ip addr show eth0 | grep -E '^\s*inet' | grep -m1 global | awk '{ print $2 }' | sed 's|/.*||')
+    DOCKER_IP="ec2-52-64-146-119.ap-southeast-2.compute.amazonaws.com"
     # KIE Workbench environment variables are set. Proceed with automatic configuration.
     echo "Detected successfull KIE Workbench container linked. Applying automatic configuration for the linked containers..."
-    export KIE_SERVER_LOCATION="http://$DOCKER_IP:8080/$KIE_CONTEXT_PATH/services/rest/server"
-    export KIE_SERVER_CONTROLLER="http://$KIE_WB_PORT_8080_TCP_ADDR:8080/$KIE_WB_ENV_KIE_CONTEXT_PATH/rest/controller"
-    export KIE_MAVEN_REPO="http://$KIE_WB_PORT_8080_TCP_ADDR:8080/$KIE_WB_ENV_KIE_CONTEXT_PATH/maven2"
+    export KIE_SERVER_LOCATION="http://$DOCKER_IP:8180/$KIE_CONTEXT_PATH/services/rest/server"
+    export KIE_SERVER_CONTROLLER="http://$DOCKER_IP:8080/$KIE_WB_ENV_KIE_CONTEXT_PATH/rest/controller"
+    export KIE_MAVEN_REPO="http://$DOCKER_IP:8080/$KIE_WB_ENV_KIE_CONTEXT_PATH/maven2"
 fi
 
 # Default arguments for running the KIE Execution server.
